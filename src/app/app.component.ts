a6b423f81f88ca7816cb4813626cee7753002e68
aysnmicah <micah.ayson1007@gmail.com>
2025-01-13 13:08:33 +0800

    ng-pipe-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
   title = 'ng-pipes-demo';
+  presentDate = new Date();
+
+  price : number = 20000; ngOnInit() {
+
+  }
+  Fruits = ['Apple', 'Banana', 'Orange','Grapes', 'Pineapple', 'Mango'];
+
+  time$ = interval(1000).pipe(map(() => new Date()));
+
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+
+  testArray = [1, 2, 3, 4, 5, 6];
+
+  testObject = {
+    name: 'Micah Ayson',
+    age: 21,
+    food: 'Kare-kare'
+  };
+
 }
