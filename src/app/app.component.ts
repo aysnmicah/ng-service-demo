c397d02cda50af17eb13aee1844d027989f935de
aysnmicah <micah.ayson1007@gmail.com>
2025-01-20 11:48:56 +0800

    ng service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { interval } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { MyserviceService } from './myservice.service';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, NewCmpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -34,5 +37,12 @@ export class AppComponent {
     age: 21,
     food: 'Kare-kare'
   };
+
+  todaydate;
+  componentproperty;
+  constructor(private myservice:MyserviceService){
+    this.todaydate = this.myservice.showTodayDate()
+    this.componentproperty = this.myservice.serviceproperty;
+  }
 
 }
